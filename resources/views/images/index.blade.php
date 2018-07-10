99695682f10b92a3229e2a4a188682b95ebc33e3
SopheakWEP <sopheak.riths@gmail.com>
2018-07-10 18:03:07 +0700

    clearn some useless file code

--- resources/views/images/index.blade.php
+++ resources/views/images/index.blade.php
@@ -1,4 +1,41 @@
 @extends('layout.app')
 @section('content')
-
+<ul id="tree2">
+    <li>TECH
+        <ul>
+            <li>Company Maintenance</li>
+            <li>Employees
+            <ul>
+                <li>Reports
+                <ul>
+                    <li>Report1</li>
+                    <li>Report2</li>
+                    <li>Report3</li>
+                </ul>
+                </li>
+                <li>Employee Maint.</li>
+            </ul>
+            </li>
+            <li>Human Resources</li>
+        </ul>
+    </li>
+    <li>XRP
+        <ul>
+            <li>Company Maintenance</li>
+            <li>Employees
+                <ul>
+                    <li>Reports
+                        <ul>
+                            <li>Report1</li>
+                            <li>Report2</li>
+                            <li>Report3</li>
+                        </ul>
+                    </li>
+                    <li>Employee Maint.</li>
+                </ul>
+            </li>
+            <li>Human Resources</li>
+        </ul>
+    </li>
+</ul> 
 @endsection
--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -5,18 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Admin LTE</title>
-    <link rel="stylesheet" href="{{asset('bower_components/bootstrap/dist/css/bootstrap.min.css')}}">
-    <link rel="stylesheet" href="{{asset('bower_components/font-awesome/css/font-awesome.min.css')}}">
-    <link rel="stylesheet" href="{{asset('bower_components/Ionicons/css/ionicons.min.css')}}">
-    <link rel="stylesheet" href="{{asset('dist/css/AdminLTE.min.css')}}">
-    <link rel="stylesheet" href="{{asset('dist/css/skins/skin-blue.css')}}">
-    <link rel="stylesheet" href="{{asset('bower_components/morris.js/morris.css')}}">
-    <link rel="stylesheet" href="{{asset('bower_components/jvectormap/jquery-jvectormap.css')}}">
-    <link rel="stylesheet" href="{{asset('bower_components/bootstrap-datepicker/dist/css/bootstrap-datepicker.min.css')}}">
-    <link rel="stylesheet" href="{{asset('bower_components/bootstrap-daterangepicker/daterangepicker.css')}}">
-    <link rel="stylesheet" href="{{asset('plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css')}}">
-    <link rel="stylesheet" href="{{asset('dist/css/skins/_all-skins.min.css')}}">
-    <link rel="stylesheet" href="{{asset('plugins/bootstrap-slider/slider.css')}}">
+    <link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}">
+    <link rel="stylesheet" href="{{asset('css/font-awesome.min.css')}}">
+    <link rel="stylesheet" href="{{asset('css/them.css')}}">
 
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
     <style>
@@ -982,7 +973,7 @@
 </div>
 <!-- ./wrapper -->
 <!-- jQuery 3 -->
-<script src="{{asset('bower_components/jquery/dist/jquery.min.js')}}"></script>
+<script src="{{asset('js/jquery.min.js')}}"></script>
 <!-- jQuery UI 1.11.4 -->
 <script src="{{asset('bower_components/jquery-ui/jquery-ui.min.js')}}"></script>
 <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
@@ -991,44 +982,13 @@
 </script>
 <!-- Bootstrap 3.3.7 -->
 <script src="{{asset('bower_components/bootstrap/dist/js/bootstrap.min.js')}}"></script>
-<!-- Morris.js charts -->
-<script src="{{asset('bower_components/raphael/raphael.min.js')}}"></script>
-<script src="{{asset('bower_components/morris.js/morris.min.js')}}"></script>
-<!-- Sparkline -->
-<script src="{{asset('bower_components/jquery-sparkline/dist/jquery.sparkline.min.js')}}"></script>
-<!-- jvectormap -->
-<script src="{{asset('plugins/jvectormap/jquery-jvectormap-1.2.2.min.js')}}"></script>
-<script src="{{asset('plugins/jvectormap/jquery-jvectormap-world-mill-en.js')}}"></script>
-<!-- jQuery Knob Chart -->
-<script src="{{asset('bower_components/jquery-knob/dist/jquery.knob.min.js')}}"></script>
-<!-- daterangepicker -->
-<script src="{{asset('bower_components/moment/min/moment.min.js')}}"></script>
-<script src="{{asset('bower_components/bootstrap-daterangepicker/daterangepicker.js')}}"></script>
-<!-- datepicker -->
-<script src="{{asset('bower_components/bootstrap-datepicker/dist/js/bootstrap-datepicker.min.js')}}"></script>
-<!-- Bootstrap WYSIHTML5 -->
-<script src="{{asset('plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min.js')}}"></script>
-<!-- Slimscroll -->
-<script src="{{asset('bower_components/jquery-slimscroll/jquery.slimscroll.min.js')}}"></script>
-<!-- FastClick -->
-<script src="{{asset('bower_components/fastclick/lib/fastclick.js')}}"></script>
-<script src="{{asset('bower_components/Chart.js/Chart.js')}}"></script>
 
 <!-- AdminLTE App -->
-<script src="{{asset('dist/js/adminlte.min.js')}}"></script>
+<script src="{{asset('js/them.js')}}"></script>
 <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
-<script src="{{asset('dist/js/pages/dashboard.js')}}"></script>
+<script src="{{asset('js/dashboard.js')}}"></script>
 <!-- AdminLTE for demo purposes -->
-<script src="{{asset('dist/js/demo.js')}}"></script>
-<script src="{{asset('bower_components/Flot/jquery.flot.js')}}"></script>
-<!-- FLOT RESIZE PLUGIN - allows the chart to redraw when the window is resized -->
-<script src="{{asset('bower_components/Flot/jquery.flot.resize.js')}}"></script>
-<!-- FLOT PIE PLUGIN - also used to draw donut charts -->
-<script src="{{asset('bower_components/Flot/jquery.flot.pie.js')}}"></script>
-<!-- FLOT CATEGORIES PLUGIN - Used to draw bar charts -->
-<script src="{{asset('bower_components/Flot/jquery.flot.categories.js')}}"></script>
-<script src="{{asset('plugins/bootstrap-slider/bootstrap-slider.js')}}"></script>
-
+<script src="{{asset('js/demo.js')}}"></script>
 <!-- page script -->
 <script src="{{asset('js/cusJs.js')}}"></script>
 </body>
