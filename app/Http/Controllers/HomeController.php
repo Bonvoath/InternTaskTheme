bebd4434e47bee6588e9b46eebd787949c58ea65
Bonvoath <bonvoathtanson@gmail.com>
2018-07-16 16:43:32 +0700

    login auth

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,50 +6,23 @@ use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Http\Response
+     */
     public function index()
     {
-        return view('home.index');
-    }
-    public function dashboard2(){
-        return view('home.dashboard2');
-    }
-    public function widgets()
-    {
-        return view('home.widgets');
-    }
-    public function chartjs()
-    {
-        return view('charts.chartjs');
-    }
-    public function morris()
-    {
-        return view('charts.morris');
-    }
-    public function flot()
-    {
-        return view('charts.flot');
-    }
-    public function inline()
-    {
-        return view('charts.inline');
-    }
-    public function general()
-    {
-        return view('UI.general');
-    }
-    public function icons()
-    {
-        return view('UI.icons');
-    }
-    public function buttons()
-    {
-        return view('UI.buttons');
-    }
-    public function slider(){
-        return view('UI.sliders');
-    }
-    public function timeline()
-    {
-        return view('UI.timeline');
+        return view('home');
     }
 }
